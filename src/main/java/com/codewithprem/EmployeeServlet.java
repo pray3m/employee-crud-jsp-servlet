0627de39681cd5abb1ec705d71ae07c2a0f4f8a6
pray3m <prem.gtm9@gmail.com>
2024-07-07 00:01:04 +0545

    Implement doDelete for deleting employees

--- src/main/java/com/codewithprem/EmployeeServlet.java
+++ src/main/java/com/codewithprem/EmployeeServlet.java
@@ -1,7 +1,9 @@
 package com.codewithprem;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -105,5 +107,28 @@ public class EmployeeServlet extends HttpServlet {
             resp.getWriter().print("Error updating employee");
         }
 
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        int employeeId = 0;
+        employeeId = Integer.parseInt(req.getParameter("id"));
+
+        if (employeeId == 0) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print("All fields are required: name, position, and salary.");
+            return;
+        }
+
+        try {
+            employeeDAO.deleteEmployee(employeeId);
+            PrintWriter out = resp.getWriter();
+            out.print("Employee deleted successfully");
+        } catch (SQLException | ClassNotFoundException e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print("Error deleting employee");
+        }
     }
 }
